2c1bfd5731911fdbbfd4b824c3284f4398b40b41
rias-z <chestermx88@gmail.com>
2018-05-16 11:59:13 +0900

    feat|fix: 画像を表示できるように変更

--- src/components/ProductBox/index.js
+++ src/components/ProductBox/index.js
@@ -1,15 +1,53 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 
-const ProductBox = (props) => {
-  const { product } = props
+class ProductBox extends Component {
+  state = {
+    imageFile: null,
+  }
 
-  return (
-    <div className="ProductBox">
-      <h3>{product.title}</h3>
-      {product.description}
-    </div>
-  )
+  componentWillMount() {
+    const { product } = this.props
+
+    import('../../static/images/products/sc_product_' + product.id + '.jpg')
+      .then(file => {
+        this.setState({
+          imageFile: file,
+        })
+      })
+      .catch(() => {
+        console.log('[Error] imageFile Not Found: product:' + product.id)
+      })
+  }
+
+  render() {
+    const { product } = this.props
+
+    return (
+      <div className="ProductBox">
+        <h3>{product.title}</h3>
+
+        {(() => {
+          if (this.state.imageFile) {
+            return (
+              <img
+                alt={product.id}
+                src={this.state.imageFile}
+                width='200'
+                height='100'
+              />
+            )
+          } else {
+            return (
+              <div>[image not found]</div>
+            )
+          }
+        })()}
+
+        {product.description}
+      </div>
+    )
+  }
 }
 
 export default ProductBox
