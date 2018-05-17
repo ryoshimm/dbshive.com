ff3ad47b5133be8374611ead112b78c64099b116
rias-z <chestermx88@gmail.com>
2018-05-17 19:55:04 +0900

    fix: 画像の表示方法の変更

--- src/components/ImageBox/index.js
+++ src/components/ImageBox/index.js
@@ -3,12 +3,14 @@ import styled from 'styled-components'
 
 const ImageWrapper = styled.div`
   width: 100%;
-  height: 219px;
+  height: 13rem;
 `
 
 const Image = styled.img`
-  max-width: 100%;
+  width: 100%;
+  height: 100%
   border-radius: 0.5rem;
+  object-fit: cover;
 `
 
 
