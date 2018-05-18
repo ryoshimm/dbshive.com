bf86d9cba05d4f93b5a9c3d8652ac9076b4ff3ff
rias-z <chestermx88@gmail.com>
2018-05-18 19:23:30 +0900

    feat: aboutページを作成

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,11 +1,44 @@
 import React from 'react'
+import styled from 'styled-components'
+
+// components
+import AuthorBox from '../../components/AuthorBox'
+
+// assets
+import { authorAsset } from '../../static/assets/authorAsset'
+
+export const StyledAbout = styled.div`
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #334;
+  color: #e1e8ed;
+`
+
+export const Wrapper = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  justify-content: center;
+  padding: 0;
+`
 
 
 const About = () => {
   return (
-    <div className="About">
+    <StyledAbout className="About">
       <h1>About</h1>
-    </div>
+      <Wrapper>
+        {authorAsset.map(author => {
+          return (
+            <AuthorBox
+              author={author}
+            />
+          )
+        })}
+      </Wrapper>
+    </StyledAbout>
   )
 }
 
