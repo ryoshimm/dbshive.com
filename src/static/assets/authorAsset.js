d1a57144e9e6019d813914c997a0367bc80c7d2e
rias-z <chestermx88@gmail.com>
2018-06-17 16:29:11 +0900

    hotfix: @atsuo1203のmail記述追加

--- src/static/assets/authorAsset.js
+++ src/static/assets/authorAsset.js
@@ -48,7 +48,7 @@ export const authorAsset = [
       '飲み会',
       'VTuber視聴',
     ],
-    mail: '*****@gmail.com',
+    mail: 'atsuo@cps.im.dendai.ac.jp',
     url: 'https://atsuo1203.github.io/',
     githubUrl: 'https://github.com/atsuo1203',
   }
